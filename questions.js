91161fbf5b5c007b625322756c95a1372f1dd6a7
Yuot Cikom Ayiei <69474820+yuotcikom@users.noreply.github.com>
2021-03-09 22:46:34 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,180 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What is the full form of CPU",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Pacific Unit",
+      "Central Processing Unit",
+      "Cars production Unit",
+      "Computer Production Unit"
+    ]
+  },
+  {
+  numb: 7,
+  question: "What is the full form of CPU",
+  answer: "Central Processing Unit",
+  options: [
+    "Central Pacific Unit",
+    "Central Processing Unit",
+    "Cars production Unit",
+    "Computer Production Unit"
+  ]
+},
+{
+numb: 8,
+question: "What is the full form of CPU",
+answer: "Central Processing Unit",
+options: [
+  "Central Pacific Unit",
+  "Central Processing Unit",
+  "Cars production Unit",
+  "Computer Production Unit"
+]
+},
+{
+numb: 9,
+question: "What is the full form of CPU",
+answer: "Central Processing Unit",
+options: [
+  "Central Pacific Unit",
+  "Central Processing Unit",
+  "Cars production Unit",
+  "Computer Production Unit"
+]
+},
+{
+numb: 10,
+question: "What is the full form of CPU",
+answer: "Central Processing Unit",
+options: [
+  "Central Pacific Unit",
+  "Central Processing Unit",
+  "Cars production Unit",
+  "Computer Production Unit"
+]
+},
+{
+numb: 11,
+question: "What is the full form of CPU",
+answer: "Central Processing Unit",
+options: [
+  "Central Pacific Unit",
+  "Central Processing Unit",
+  "Cars production Unit",
+  "Computer Production Unit"
+]
+},
+{
+numb: 12,
+question: "What is the full form of CPU",
+answer: "Central Processing Unit",
+options: [
+  "Central Pacific Unit",
+  "Central Processing Unit",
+  "Cars production Unit",
+  "Computer Production Unit"
+]
+},
+{
+numb: 13,
+question: "Why did Thailand and Hong Kong used three fingers salute?",
+answer: "To symbolize resistance",
+options: [
+  "To ignore the person",
+  "To symbolize resistance",
+  "To give credits",
+  "It's a custom and norms"
+]
+},
+{
+numb: 14,
+question: "When was the three-fingers salute used?",
+answer: "In 2014 against anti-coup protest",
+options: [
+  "In 2014 and 2016",
+  "In 2019 and 2020",
+  "In 2014 against anti-coup protest",
+  "In Thailand 2015"
+]
+},
+{
+numb: 15,
+question: "What is the Grand Ethiopian Renaissance Dam (GERD) project for?",
+answer: "To generate electricity to relieve Ethiopia's acute energy shortage ",
+options: [
+  "To help in controlling floods",
+  "To provide free power to African countries",
+  "To generate electricity to relieve Ethiopia's acute energy shortage ",
+  "To control waterflow"
+]
+},
+{
+numb: 16,
+question: "What is WMR infull?",
+answer: "World Malaria Report",
+options: [
+  "World Magazine Report",
+  "World Malaria Report",
+  "World Management Rehabilitation",
+  "World Man-day report"
+]
+},
+];
